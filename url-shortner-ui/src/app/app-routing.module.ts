7855f66ed502b1484d8bd5079373dcf404ed0c23
harshaldhake123 <harshaldhake123@gmail.com>
2025-09-13 17:11:56 +0530

    add boilerplate landing page

--- url-shortner-ui/src/app/app-routing.module.ts
+++ url-shortner-ui/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: 'home',
+		component: HomeComponent,
+		title: 'Home',
+	},
+	{
+		path: '',
+		redirectTo: 'home',
+		pathMatch: 'full',
+	},
+	{
+		path: '*',
+		redirectTo: 'home',
+	}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
